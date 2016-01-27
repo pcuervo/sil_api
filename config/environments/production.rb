227db1c58e0654a58452b0aa67813586372f109e
maichomper <maichomper@gmail.com>
2016-01-27 10:34:08 -0600

    move aws-sdk to gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.time_zone = "Mexico City"
+  
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
