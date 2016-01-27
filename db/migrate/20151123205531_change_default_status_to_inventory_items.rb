dedd5ab576ac444f165cbf7dffa9d1304401c8df
maichomper <maichomper@gmail.com>
2016-01-26 18:59:00 -0600

    remove migration to convert status to int

--- db/migrate/20151123205531_change_default_status_to_inventory_items.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDefaultStatusToInventoryItems < ActiveRecord::Migration
-  def change
-    change_table :inventory_items do |t|
-      change_column :inventory_items, :status, :integer, :default => 1
-    end
-  end
-end
