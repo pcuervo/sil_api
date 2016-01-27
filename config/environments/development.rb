21d5c1adcb8351befff71f84946e1a114edd72d0
maichomper <maichomper@gmail.com>
2016-01-27 10:34:08 -0600

    move aws-sdk to gitignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,13 @@ Rails.application.configure do
   # Fixed time zone for Mexico City
   config.time_zone = "Mexico City"
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
