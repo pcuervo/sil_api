a3e9ed79d6d8d0879a379ec0ffe0690259c70443
maichomper <maichomper@gmail.com>
2016-01-27 10:54:41 -0600

    change config for ENV S3 vars

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,7 +1,8 @@
 if Rails.env == "production"
-  S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_KEY'], :bucket => "sil-dev"} 
+  S3_CREDENTIALS = { :access_key_id => ENV['AWSAccessKeyId'], :secret_access_key => ENV['AWSSecretKey'], :bucket => ENV['S3_BUCKET_NAME']} 
+  puts S3_CREDENTIALS.to_yaml
 
  else     
     S3_CREDENTIALS = Rails.root.join("config/s3.yml")
-    puts S3_CREDENTIALS.to_yaml
+    
 end
