6eb297f4b8ebe2f759784d485cc5cff4d346168f
maichomper <maichomper@gmail.com>
2016-04-25 18:10:36 -0500

    adds pending entry notifications to bundle item

--- db/migrate/20160329231327_create_notifications.rb
+++ db/migrate/20160329231327_create_notifications.rb
@@ -2,8 +2,8 @@ class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
       t.string      :message
-      t.integer     :inventory_item
-      t.integer      :status, default: 1
+      t.integer     :inventory_item_id
+      t.integer     :status, default: 1
       t.timestamps  null: false
     end
   end
--- spec/factories/inventory_item_requests.rb
+++ spec/factories/inventory_item_requests.rb
@@ -4,10 +4,10 @@ FactoryGirl.define do
     description               { FFaker::HipsterIpsum.phrase } 
     quantity                  10
     item_type                 'Laptop'
-    project_id                15
-    pm_id                     2
-    ae_id                     5
-    state                     'Nuevo'
+    project_id                1
+    pm_id                     3
+    ae_id                     4
+    state                     1
     validity_expiration_date  '2016-09-29'
     entry_date                '2016-09-29'
   end
