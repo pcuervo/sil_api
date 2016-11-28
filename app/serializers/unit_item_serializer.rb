a67e92ba76d21a7d265a2efd224b3fe772ff06fc
maichomper <maichomper@gmail.com>
2016-11-28 13:21:23 -0600

    Add InventoryItem id to UnitItem serializer

--- app/serializers/unit_item_serializer.rb
+++ app/serializers/unit_item_serializer.rb
@@ -1,4 +1,9 @@
 class UnitItemSerializer < ActiveModel::Serializer
-  attributes :id, :serial_number, :brand, :model, :name, :description, :status, :barcode, :state, :value, :actable_type, :actable_id, :item_type, :created_at
+  attributes :id, :serial_number, :brand, :model, :name, :description, :status, :barcode, :state, :value, :actable_type, :actable_id, :item_type, :created_at, :inventory_item_id
   has_one :project
+
+  def inventory_item_id
+    item = InventoryItem.where('actable_id = ? and actable_type = "UnitItem"', object.id )
+    item.id
+  end
 end
