1b52b6a5866ec19b24241b2b0db6f2b08d28dcf4
maichomper <maichomper@gmail.com>
2016-11-28 13:12:16 -0600

    Add created_at and item_type to UnitItemSerializer

--- app/serializers/unit_item_serializer.rb
+++ app/serializers/unit_item_serializer.rb
@@ -1,4 +1,4 @@
 class UnitItemSerializer < ActiveModel::Serializer
-  attributes :id, :serial_number, :brand, :model, :name, :description, :status, :barcode, :state, :value, :actable_type, :actable_id
+  attributes :id, :serial_number, :brand, :model, :name, :description, :status, :barcode, :state, :value, :actable_type, :actable_id, :item_type, :created_at
   has_one :project
 end
