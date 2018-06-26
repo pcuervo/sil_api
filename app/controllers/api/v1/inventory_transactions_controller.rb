44acef993be5045460bdfce44c4b442a27df78ed
maichomper <maichomper@gmail.com>
2018-06-26 10:41:05 -0500

    Fix error last_checkin_folio

--- app/controllers/api/v1/inventory_transactions_controller.rb
+++ app/controllers/api/v1/inventory_transactions_controller.rb
@@ -35,9 +35,9 @@ module Api
       end
 
       def last_checkin_folio
-        last_folio = CheckInTransaction.where('folio != ?', '-').last.folio
-        return render json: {folio: last_folio}, status: :ok if last_folio.present?
-        
+        last_folio = CheckInTransaction.where('folio != ?', '-').last
+        return render json: {folio: last_folio.last}, status: :ok if last_folio.present?
+
         render json: {folio: "FE-0000000"}, status: :ok
       end
 
