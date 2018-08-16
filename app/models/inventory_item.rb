570d8997e1fbcb9e5bef9d85d53bb480956629f6
maichomper <maichomper@gmail.com>
2018-08-16 16:57:54 -0500

    Agregar busqueda por num serie

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -54,7 +54,9 @@ class InventoryItem < ActiveRecord::Base # rubocop:disable Metrics/ClassLength
     inventory_items = inventory_items.out_of_stock if params[:out_of_stock].present?
     inventory_items_details = { 'inventory_items' => [] }
 
-    inventory_items = inventory_items.where('lower(name) LIKE ? OR lower(barcode) LIKE ? OR lower(serial_number) LIKE ?', "%#{params[:keyword].downcase}%", "%#{params[:keyword].downcase}%", "%#{params[:keyword].downcase}%") if params[:keyword]
+    inventory_items = inventory_items.where('lower(name) LIKE ? OR lower(barcode) LIKE ? OR lower(serial_number) LIKE ?', "%#{params[:keyword].downcase}%", "%#{params[:keyword].downcase}%") if params[:keyword]
+
+    inventory_items = inventory_items.where('lower(serial_number) LIKE ?', "%#{params[:serial_number].downcase}%") if params[:serial_number]
 
     inventory_items = inventory_items.where('project_id = ?', params[:project_id]) if params[:project_id].present?
 
