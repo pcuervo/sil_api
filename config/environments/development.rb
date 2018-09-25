47d0bae47e11f6ebe60a09b747a3d64f9424f57f
maichomper <maichomper@gmail.com>
2018-09-25 00:43:50 -0500

    Fix S3 issues with Paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,6 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :region => 'us-east-1'
+    :s3_region => 'us-east-1'
   }
 end
