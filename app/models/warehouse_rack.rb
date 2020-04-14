04867807ad43869b514a860ed9c158b2c57bfcfc
maichomper <maichomper@gmail.com>
2020-04-14 09:01:57 -0500

    Add SKU to warehouse items

--- app/models/warehouse_rack.rb
+++ app/models/warehouse_rack.rb
@@ -79,6 +79,7 @@ class WarehouseRack < ActiveRecord::Base
           'img'           => item.item_img(:medium),
           'thumb'         => item.item_img(:thumb),
           'name'          => item.name,
+          'brand'         => item.brand,
           'location_id'   => il.warehouse_location_id,
           'location'      => il.warehouse_location.name,
           'quantity'      => il.quantity,
